1da2fc42471a84f7331c6604f401c2404d9db3c8
Nico Loparco <nicoloparco75@gmail.com>
2019-11-11 22:46:34 -0500

    partially completed javascript

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,53 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Chicago4$",
+    database: "bamazonDB"
+})
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log(`Connected as ID ${connection.threadId}`);
+    loadDatabase();
+});
+
+function loadDatabase() {
+    connection.query("SELECT * FROM products", function (err, response) {
+        if (err) throw err;
+        console.log(response);
+
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the ID number of the item you would like to buy?",
+                name: "item"
+            },
+            {
+                type: "input",
+                messsage: "How many of the item would you like to buy?",
+                name: "quantity"
+            }
+        ]).then(function (response) {
+            console.log(response)
+        })
+    })
+};
+
+// inquirer.prompt([
+//     {
+//         type: "input",
+//         message: "What is the ID number of the item you would like to buy?",
+//         name: "item"
+//     },
+//     {
+//         type: "input",
+//         messsage: "How many of the item would you like to buy?",
+//         name: "quantity"
+//     }
+// ]).then(function (response) {
+//     console.log(response)
+// })
