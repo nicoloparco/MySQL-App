50fc7f0d277353fe403680db9fa5505d956858b2
Nico Loparco <nicoloparco75@gmail.com>
2019-11-12 23:37:26 -0500

    Completed mySQL and JavaScript interactivity. Launching completed v(1.0)

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,12 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table2");
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Chicago4$",
+    password: "docker",
     database: "bamazonDB"
 })
 
@@ -18,36 +19,95 @@ connection.connect(function (err) {
 function loadDatabase() {
     connection.query("SELECT * FROM products", function (err, response) {
         if (err) throw err;
-        console.log(response);
+        console.log("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
+        console.log("Welcome to the Bamazon Store!");
+        console.log("");
+        console.log("Below is a list of all items and their respective quantities");
+        console.log("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
 
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "What is the ID number of the item you would like to buy?",
-                name: "item"
-            },
-            {
-                type: "input",
-                messsage: "How many of the item would you like to buy?",
-                name: "quantity"
+
+        var table = new Table({
+            head: ["Product Id", "Product Name", "Department Name", "Price", "Quantity"],
+            colWidths: [20, 100, 30, 10, 10],
+            colAligns: ["center"],
+            style: {
+                head: ["aqua"],
+                compact: true
             }
-        ]).then(function (response) {
-            console.log(response)
-        })
-    })
+        });
+
+        for (var i = 0; i < response.length; i++) {
+            table.push([
+                response[i].id,
+                response[i].product_name,
+                response[i].department_name,
+                response[i].price,
+                response[i].stock_quantity])
+        };
+
+        console.log(table.toString());
+        console.log("");
+    });
+
 };
 
-// inquirer.prompt([
-//     {
-//         type: "input",
-//         message: "What is the ID number of the item you would like to buy?",
-//         name: "item"
-//     },
-//     {
-//         type: "input",
-//         messsage: "How many of the item would you like to buy?",
-//         name: "quantity"
-//     }
-// ]).then(function (response) {
-//     console.log(response)
-// })
+loadDatabase();
+
+var customer = function () {
+    inquirer.prompt(
+        {
+            type: "input",
+            message: "What is the ID number of the item you would like to buy?",
+            name: "item"
+        }
+    ).then(function (firstAnswer) {
+
+        var productID = firstAnswer.item
+
+        connection.query(`SELECT * FROM products WHERE Id=${productID}`, function (err, res) {
+            if (err) throw err;
+            if (res.length === 0) {
+                console.log("This product is out of stock or does not exist, please try a different ID number")
+                customer();
+            } else {
+                inquirer.prompt(
+                    {
+                        type: "input",
+                        message: "How many of this item would you like to purchase?",
+                        name: "quantity"
+                    }).then(function (secondAnswer) {
+                        console.log(secondAnswer)
+                        var productQuantity = secondAnswer.quantity
+
+                        if (productQuantity > res[0].stock_quantity) {
+                            console.log(`Sorry, we only have ${res[0].stock_quantity} left`)
+                            customer();
+                        } else {
+                            console.log("")
+                            console.log(`${res[0].product_name} purchased`);
+                            console.log(`${productQuantity} @ ${res[0].price}`);
+
+                            var newQuantity = res[0].stock_quantity - productQuantity;
+                            connection.query(
+                                "UPDATE products SET stock_quantity = " +
+                                newQuantity +
+                                " WHERE Id = " +
+                                res[0].id,
+
+                                function (err, res) {
+                                    if (err) throw err;
+                                    console.log("")
+                                    console.log("Your purchase has been made")
+                                    customer();
+                                    loadDatabase();
+                                    connection.end
+                                    }
+                                );
+                            }
+                        });
+                    }
+            });
+        });
+    };
+
+customer();
