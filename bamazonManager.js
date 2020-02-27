fcccf9a6262b7c6f8828d1f8f5a839aea51d7141
Nico Loparco <nicoloparco75@gmail.com>
2020-02-26 21:21:39 -0500

    added add new product feature to manager level

--- bamazonManager.js
+++ bamazonManager.js
@@ -114,9 +114,49 @@ function viewLowInventory() {
 };
 
 function addToInventory() {
-
+    
 };
 
 function addNewProduct() {
+    inquirer.prompt({
+        type: "input",
+        message: 'Please Enter Information About Product ("name of product", "department of product", "price", "quantity")',
+        name: "product"
+    }).then(function(answer){ 
+        
+        var product = answer.product
 
+        connection.query(`INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (${product})`).then(
+            
+            connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+            
+            var table = new Table({
+                head: ["Product Id", "Product Name", "Department Name", "Product Price", "Product Quantity"],
+                colWidths: [20, 100, 30, 20, 20],
+                colAlign: "center",
+                style: {
+                    head: ["aqua"],
+                    compact: true
+                }
+            });
+
+            for (var i = 0; i < res.length; i++ ){
+                table.push([
+                    res[i].id,
+                    res[i].product_name,
+                    res[i].department_name,
+                    res[i].price,
+                    res[i].stock_quantity,
+                ])
+            };
+    
+            console.log(table.toString());
+            console.log("")
+            })
+        )
+    })
 }
+
+
+
